b24862d99062f06c83137410b1ea47bc40eda5c2
Sergey <xxsmotur@gmail.com>
2019-11-24 18:34:46 +0500

    update gitlab actions and tests

--- counter_test.go
+++ counter_test.go
@@ -11,3 +11,11 @@ func TestIncCounter(t *testing.T) {
 	c.Inc(1)
 	assert.Equal(t, c.Count(), uint(2))
 }
+
+func TestFewCount(t *testing.T) {
+	c := NewCounter()
+	c.Inc(1)
+	c.Inc(10)
+	assert.Error(t, c.Inc(0))
+	assert.Equal(t, c.Count(), uint(11))
+}
