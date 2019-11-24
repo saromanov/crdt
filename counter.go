d421f1b474e7ca382cb98d21e36dc9bec59948df
Sergey <xxsmotur@gmail.com>
2019-11-24 18:01:11 +0500

    Fixes

--- counter.go
+++ counter.go
@@ -1,7 +1,11 @@
 package crdt
 
+import (
+	"fmt"
+)
+
 type Counter struct {
-	id string
+	id   string
 	data map[string]uint
 }
 
@@ -11,25 +15,28 @@ func NewCounter() *Counter {
 	}
 }
 
-func (g *Counter) Inc(incr int) {
-	g.counter[g.id] += incr
+// Inc provides incrementation of the counter
+func (g *Counter) Inc(incr uint) error {
+	if incr == 0 {
+		return fmt.Errorf("incr value can't be zero")
+	}
+	g.data[g.id] += incr
+	return nil
 }
 
-func (g *Counter) Count() (total int) {
-	for _, val := range g.counter {
+func (g *Counter) Count() uint {
+	var total uint
+	for _, val := range g.data {
 		total += val
 	}
-	return
+	return total
 }
 
 // Merge provides merging of two counters
-func (g *Counter) Merge(c *Counter) uint {
-	var total uint
+func (g *Counter) Merge(c *Counter){
 	for ident, val := range c.data {
 		if v, ok := g.data[ident]; !ok || v < val {
 			g.data[ident] = val
 		}
 	}
-
-	return total
 }
