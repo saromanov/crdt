ad9e5653f16d040d915632d4c3c93fedd60e595b
Sergey <xxsmotur@gmail.com>
2019-11-26 18:38:20 +0500

    Implementation of TwoPSet

--- set/2p_set.go
+++ set/2p_set.go
@@ -1,1 +1,27 @@
 package set
+
+type TwoPSet struct {
+	added   setObject
+	removed setObject
+}
+
+func NewTwoPSet() *TwoPSet {
+	return &TwoPSet{
+		added:   make(setObject),
+		removed: make(setObject),
+	}
+}
+
+func (g *TwoPSet) Add(value interface{}) {
+	g.added[value] = "data"
+}
+
+func (g *TwoPSet) Remove(value interface{}) {
+	g.removed[value] = "data"
+}
+
+func (g *TwoPSet) Lookup(value interface{}) bool {
+	_, ok := g.added[value]
+	_, okRem := g.removed[value]
+	return ok && !okRem
+}
