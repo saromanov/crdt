a1e5ee5fb155d019844199dd1418cb08d2378b6a
Sergey <xxsmotur@gmail.com>
2019-11-27 12:43:35 +0500

    implementation of or set

---
+++ set/or_set.go
@@ -0,0 +1,45 @@
+package set
+
+import uuid "github.com/satori/go.uuid"
+
+type ORSet struct {
+	add     map[interface{}]map[string]struct{}
+	removed map[interface{}]map[string]struct{}
+}
+
+// NewORSet provides creating of the OrSet
+func NewORSet() *ORSet {
+	return &ORSet{
+		add:     make(map[interface{}]map[string]struct{}),
+		removed: make(map[interface{}]map[string]struct{}),
+	}
+}
+
+// Add provides adding element to the set
+func (o *ORSet) Add(value interface{}) {
+	if m, ok := o.add[value]; ok {
+		m[uuid.NewV4().String()] = struct{}{}
+		o.add[value] = m
+		return
+	}
+
+	m := make(map[string]struct{})
+
+	m[uuid.NewV4().String()] = struct{}{}
+	o.add[value] = m
+}
+
+func (o *ORSet) Remove(value interface{}) {
+	r, ok := o.removed[value]
+	if !ok {
+		r = make(map[string]struct{})
+	}
+
+	if m, ok := o.add[value]; ok {
+		for uid, _ := range m {
+			r[uid] = struct{}{}
+		}
+	}
+
+	o.removed[value] = r
+}
