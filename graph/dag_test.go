a0df96d79d15221b12197a61425d36c693068ead
Sergey <xxsmotur@gmail.com>
2019-11-28 00:42:41 +0500

    Add more tests

--- graph/dag_test.go
+++ graph/dag_test.go
@@ -11,4 +11,7 @@ func TestAddEdge(t *testing.T) {
 	s.AddEdge("a", "b")
 	s.AddEdge("a", "c")
 	assert.Equal(t, s.LookupVertex("a"), true)
+	assert.Equal(t, s.LookupVertex("e"), false)
+	assert.Equal(t, s.LookupEdge("a", "b"), true)
+	assert.Equal(t, s.LookupEdge("b", "c"), false)
 }
